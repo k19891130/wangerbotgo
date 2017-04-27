f48bdceaf3bd1035185e5d78dc87ba10a943e57a
kevin <k19891130@gmail.com>
2017-04-27 14:04:06 +0800

    aaa

--- main.go
+++ main.go
@@ -22,9 +22,13 @@ import (
 )
 
 var bot *linebot.Client
+var boyMapping map[string]string
+var girlMapping map[string]string
 
 func main() {
 	var err error
+	boyMapping = make(map[string]string)
+	girlMapping = make(map[string]string)
 	bot, err = linebot.New(os.Getenv("ChannelSecret"), os.Getenv("ChannelAccessToken"))
 	log.Println("Bot:", bot, " err:", err)
 	http.HandleFunc("/callback", callbackHandler)
@@ -59,16 +63,39 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(event.Source.UserID + " : " + message.Text+" 裡面有#")).Do(); err != nil {
 					log.Print(err)
 					}
-				} else if strings.Contains(message.Text, "button") {
-					leftBtn := linebot.NewMessageTemplateAction("我是男生", "配對中")
-					rightBtn := linebot.NewMessageTemplateAction("我是女生", "配對中")
+				} else if message.Text == "隨機聊天" {
+					leftBtn := linebot.NewMessageTemplateAction("我是男生", "-我是男生，開始尋找配對中...")
+					rightBtn := linebot.NewMessageTemplateAction("我是女生", "-我是女生，開始尋找配對中...")
 					template := linebot.NewConfirmTemplate("隨機聊天", leftBtn, rightBtn)
+
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTemplateMessage("開始隨機聊天。", template)).Do(); err != nil {
+					log.Print(err)
+					}
+				} else if message.Text == "-我是男生，開始尋找配對中..." {
+					
+					boyMapping[event.Source.UserID] := ""
+
+					for girlId := range girlMapping {
+					    
+					    if girlMapping[girlId] == "" {
+					    	girlMapping[girlId] = event.Source.UserID
+					    	boyMapping[event.Source.UserID] = girlId
+					    }
+					}
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTemplateMessage("開始隨機聊天。", template)).Do(); err != nil {
+					log.Print(err)
+					}
+				} else if message.Text == "-我是女生，開始尋找配對中..." {
+					
 
 					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTemplateMessage("開始隨機聊天。", template)).Do(); err != nil {
 					log.Print(err)
 					}
 				} else if strings.Contains(message.Text, "**") {
 
+					if _, err := bot.PushMessage("U19cf2d03ac94f9ecc9c3ec3664b80f6d", linebot.NewTextMessage("私訊你" + event.Source.UserID)).Do(); err != nil {
+					log.Print(err)
+					}
 					if _, err := bot.PushMessage("U19cf2d03ac94f9ecc9c3ec3664b80f6d", linebot.NewTextMessage("私訊你" + event.Source.UserID)).Do(); err != nil {
 					log.Print(err)
 					}
